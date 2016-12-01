d160662e9a78cb0f97bbc76a1b0496c0f48b2b29
Ben Lipson <bjlipson@spscommerce.com>
2016-12-01 15:29:24 -0600

    make it pretty

--- server.js
+++ server.js
@@ -1,88 +1,136 @@
-var login = require("facebook-chat-api");
-var http = require('http');
-var fs = require('fs');
-var download = require('download-file')
+const login  = require('facebook-chat-api'),
+    http     = require('http'),
+    fs       = require('fs'),
+    download = require('download-file');
 
-login({email: "gifbotapi@gmail.com", password: "imnottellingyouthis"}, function callback (err, api) {
-    if(err) return console.error(err);
- 
-    api.setOptions({listenEvents: true});
- 
-    var stopListening = api.listen(function(err, event) {
-        if(err) return console.error(err);
- 
-        switch(event.type) {
-          case "message":
-            let parsedData = 'IMAGE NOT FOUND OR SOMETHING';
-            if(event.body.split(' ')[0] && event.body.split(' ')[0] === 'GIF') {
-              console.log('GETTING GIF FOR: ', event.body.split(' ')[1]);
+login({ email: 'gifbotapi@gmail.com', password: 'gifbotpassword' }, (err, api) => {
+    if (err) {
+        return console.error(err);
+    }
 
-              http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + event.body.split(' ')[1], function(res) {
-                const statusCode = res.statusCode;
-                const contentType = res.headers['content-type'];
+    api.setOptions({ listenEvents: true });
 
-                let error;
-                if (statusCode !== 200) {
-                  error = new Error(`Request Failed.\n` +
-                                    `Status Code: ${statusCode}`);
-                } else if (!/^application\/json/.test(contentType)) {
-                  error = new Error(`Invalid content-type.\n` +
-                                    `Expected application/json but received ${contentType}`);
-                }
-                if (error) {
-                  console.log(error.message);
-                  // consume response data to free up memory
-                  res.resume();
-                  return;
+    api.listen((err, event) => {
+        if (err) {
+            return console.error(err);
+        }
+
+        let parsedData = {};
+        if (event.body) {
+            let search = event.body.split(' ');
+            if (search[0].toLowerCase() === 'gif') {
+                search.shift();
+                const gifbotResponse = search.join(' ');
+                search = search.join('+');
+
+                if (search.includes('/') || search.includes('\\')) {
+                    api.sendMessage('That was very naughty what you did there', event.threadID);
+                    return;
                 }
 
-                res.setEncoding('utf8');
-                let rawData = '';
-                res.on('data', (chunk) => rawData += chunk);
-                res.on('end', () => {
-                  try {
-                    parsedData = JSON.parse(rawData);
-                    console.log(parsedData.data.url.toString());
-                    const fn = event.body.split(' ')[1] + '.gif';
-                    
-                    var options = {
-                      directory: '.',
-                      filename: fn
-                    };
+                console.log('Retrieving gif for: ', gifbotResponse);
 
-                    download(parsedData.data.image_url.toString(), options, function(err){
-                      if (err) throw err
-                      console.log("file downloaded");
-                      var msg = {
-                        body: "GIF for " + event.body.split(' ')[1] + ":",
-                        attachment: fs.createReadStream(fn)
-                      };
+                http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + search, (res) => {
+                    const statusCode = res.statusCode,
+                        contentType = res.headers['content-type'];
+                    let error;
+                    if (statusCode !== 200) {
+                        api.sendMessage('I didnt find anything, try searching better idiot', event.threadID);
+                        error = new Error(`Request Failed.\n` +
+                                          `Status Code: ${statusCode}`);
+                    } else if (!/^application\/json/.test(contentType)) {
+                        error = new Error(`Invalid content-type.\n` +
+                                          `Expected application/json but received ${contentType}`);
+                    }
 
-                      api.sendMessage(msg, event.threadID);
+                    if (error) {
+                        console.log(error.message);
+                        res.resume();
+                        return;
+                    }
 
-                      fs.unlink(fn, function (err) {
-                        if (err) console.log(err);
-                        console.log('no error on delete');
-                      });
+                    res.setEncoding('utf8');
+                    let rawData = '';
+                    res.on('data', (chunk) => {
+                        rawData += chunk;
                     });
-                  } catch (e) {
-                    console.log(e.message);
-                  }
-                });
-              }).on('error', (e) => {
-                console.log(`Got error: ${e.message}`);
-              });
 
-              api.markAsRead(event.threadID, function(err) {
-                if(err) console.log(err);
-              });
-            } else if (event.body.split(' ')[0] === 'GIF') {
-              api.sendMessage(parsedData, event.threadID);
+                    res.on('end', () => {
+                        try {
+                            parsedData = JSON.parse(rawData);
+                            if (!parsedData.data.image_url) {
+                                api.sendMessage('I didnt find anything, try searching better idiot', event.threadID);
+                                return;
+                            }
+                            console.log('it has ', parsedData.data.image_frames, 'frames');
+                            const fn = event.body.split(' ')[1] + '.gif';
+                            const options = {
+                                directory: '.',
+                                filename:  fn
+                            };
+
+                            if (parsedData.data.image_frames < 100) {
+                                download(parsedData.data.image_url, options, (err) => {
+                                    if (err) {
+                                        console.log(err);
+                                        return;
+                                    }
+
+                                    console.log('file downloaded');
+                                    const msg = {
+                                        body:       'GIF for ' + gifbotResponse + ':',
+                                        attachment: fs.createReadStream(fn)
+                                    };
+
+                                    api.sendMessage(msg, event.threadID);
+
+                                    fs.unlink(fn, (err) => {
+                                        if (err) {
+                                            console.log(err);
+                                            return;
+                                        }
+
+                                        console.log('file deleted from local machine');
+                                    });
+                                });
+                            } else {
+                                download(parsedData.data.fixed_width_small_url.toString(), options, (err) => {
+                                    if (err) {
+                                        console.log(err);
+                                        return;
+                                    }
+                                    console.log('file downloaded');
+                                    const msg = {
+                                        body:       'GIF for ' + gifbotResponse + ':',
+                                        attachment: fs.createReadStream(fn)
+                                    };
+
+                                    api.sendMessage(msg, event.threadID);
+
+                                    fs.unlink(fn, (err) => {
+                                        if (err) {
+                                            console.log(err);
+                                            return;
+                                        }
+                                        console.log('file deleted from local machine');
+                                    });
+                                });
+                            }
+
+                        } catch (e) {
+                            console.log(e.message);
+                        }
+                    });
+                }).on('error', (e) => {
+                    console.log(`Got error: ${e.message}`);
+                });
+
+                api.markAsRead(event.threadID, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
             }
-            break;
-          case "event":
-            console.log(event);
-            break;
         }
     });
 });
